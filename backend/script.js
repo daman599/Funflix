d1d0f1175144a350d08537ef878479324f9bb4d5
daman599 <damanpreetk117@gmail.com>
2025-03-26 00:28:59 +0530

    added trending movies

--- backend/script.js
+++ backend/script.js
@@ -1,11 +1,11 @@
 const apikey='69318cb897ca41bb2651034004fc0ed0';
-const url=`https://api.themoviedb.org/3/movie/550?api_key=${apikey}`;
+const url=`https://api.themoviedb.org/3/trending/movie/day?api_key=${apikey}`;
 
 async function movieData(){
    console.log("lets go")
    const response=await fetch(url)
    const data=await response.json();
-   console.log(data.overview);
+   console.log(data);
 }
 movieData();
 
