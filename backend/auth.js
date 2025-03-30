27d127727e534116c89baac3a540b0ed49716d4f
daman599 <damanpreetk117@gmail.com>
2025-03-30 22:40:33 +0530

    connect FE to BE

--- backend/auth.js
+++ backend/auth.js
@@ -1,75 +1,61 @@
 const express=require("express");
 const jwt=require("jsonwebtoken");
-const JWT_KEY="hfkwerwircncsdfj";
+const JWT_KEY="daman@1234";
 const app=express();
+const cors=require("cors");
+app.use(cors());
 
-function auth(req,res,next){
-   const token=req.headers.token;
-   if(!token){
-      return res.send("not logged in");
-   }
-   try{
-   const verifyInfo=jwt.verify(token,JWT_KEY);
-   const name=verifyInfo.username;
-   if(name){
-      req.username=name;
-      next();
-   }
-   }
-   catch(err){
-      return res.status(404).json({message:"invalid token"})
-   }
-
-}
+const users=[];
 app.use(express.json());
-let users=[];
-app.post('/signup',function(req,res){
+function auth(req,res,next){
+    const token= req.headers.token;
+    if(!token){
+        return res.send("not logged in")
+    }
+try{
+    const verifiedData=jwt.verify(token,JWT_KEY);
+    const username=verifiedData.username;
+    req.username=username;
+    next();
+}
+catch(err){
+      return res.status(404).send("invalid token")
+ }        
+}
+
+app.post('/signup',function (req,res){
    const username=req.body.username;
    const password=req.body.password;
-   
    users.push({
-      username:username,
-      password:password
+    username:username,
+    password:password
    })
-   console.log(users)
-
    res.send("you are signed up");
 })
 
-app.post('/login',function(req,res){
-   const username=req.body.username;
-   const password=req.body.password;
-   
-   let foundUser= users.find((user)=>user.username == username && user.password == password)
-   if(foundUser){
-   const token=jwt.sign({
-         username
-        },JWT_KEY)
-   res.json({
-      "token":token
-      })
-   }
-   else{
+app.post('/login',function (req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+    
+    const foundUser=users.find((user) => user.username==username && user.password == password);
+    if(foundUser){
+      const token=jwt.sign({
+        username
+      },JWT_KEY);
       res.json({
-         message:"wrong credantials"
+        "token":token
       })
-   }
+    }
+    else{
+        res.send("wrong credantials");
+    }
+ })
+
+app.get('/me',auth ,function(req,res){
+    res.json({
+    username:req.username
+  })
 })
-
-app.get('/me',auth,function(req,res){
-
- const foundUser= users.find((user)=> user.username == req.username);
- if(foundUser){
-   res.json({
-      username:foundUser.username,
-      password:foundUser.password
-   })
- }
-
+app.listen(3000,()=>{
+    console.log("server has started");
 })
-
-app.listen(3000,function(){
-   console.log("Server is up");
-})
-
-
