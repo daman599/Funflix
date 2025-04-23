d3d69a0a2e5ea320e6cc932d1209ba147ced1f3a
daman599 <damanpreetk117@gmail.com>
2025-04-23 23:55:27 +0530

    added more schemas related to searching

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,42 @@
 const mongoose = require("mongoose");
-const Schema=mongoose.Schema;
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
 
 const User=new Schema({
     username:String,
     email:{type:String,unique:true,required:true},
     password:String,
 })
+const Movie_TVshow = new Schema({
+    title:{type:String,required:true},
+    content_type:String,
+    release_year:Number,
+    posterURL:String,
+    overview:String
+})
+const Streaming_Service = new Schema({
+     serviceName:{type:String,required:true},
+     logoURL:String,
+     websiteURL:String
+})
+const Availability = new Schema({
+    movieId:{type:ObjectId,Required:true},
+    serviceId:{type:ObjectId,Required:true},
+    regionCode:String,
+    availability_type:String,
+})
 
-const UserModel=mongoose.model("UserInfo",User);
+Availability.index({ movieId: 1, regionCode: 1 }); 
 
-module.exports= UserModel
+const UserModel = mongoose.model("UserInfo",User);
+const MovieModel = mongoose.model("Movie_tvInfo",Movie_TVshow);
+const ServiceModel = mongoose.model("Streaming_Service",Streaming_Service);
+const AvailabilityModel = mongoose.model("Availability",Availability);
+
+module.exports={
+    UserModel:UserModel,
+    MovieModel:MovieModel,
+    ServiceModel:ServiceModel,
+    AvailabilityModel:AvailabilityModel
+}
+
