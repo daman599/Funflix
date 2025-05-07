41566711a64fff632bd9fb62ff9dc8656899d715
daman599 <damanpreetk117@gmail.com>
2025-05-07 23:12:15 +0530

    added trending movies endpoint

--- backend/Routes/Movie.js
+++ backend/Routes/Movie.js
@@ -7,102 +7,32 @@ const axios = require("axios");
 const { MovieModel } = require('../db');
 
 movieRouter.get('/trending',async (req,res)=>{
-   
-   const trendingMovies = await axios.get("https://api.themoviedb.org/3/trending/movie/day",{
-       params:{
-        api_key:TMDB_API_KEY
-       }
-    })
-
-    let trendMovieArr = trendingMovies.data.results;
-
-    await Promise.all(trendMovieArr.map(async (movie)=>{
-        return(
-         await MovieModel.create({
-            title:movie.title,
-            tmdb_id:movie.id,
-            type:movie.media_type,
-            release_date:movie.release_date,
-            poster_path:movie.poster_path,
-            overview:movie.overview,
-            rating:movie.vote_average
-        })
-    );
-    }))
-
-    res.json({
-      trendingMovies:trendMovieArr
-    })
-})
-
-movieRouter.get('/search',async (req,res)=>{
-   const title = req.query.title;
-   
-   const movies = await MovieModel.find({
-     title: { $regex: title, $options: 'i' }
-    })
-
-   if(movies.length > 0){
-    return res.json({
-        movies:movies
-    })
-   }
-
-   const response = await axios.get('https://api.themoviedb.org/3/search/movie',{
-     params:{
-        api_key:TMDB_API_KEY,
-        query:title
-     }
+   const url='https://api.themoviedb.org/3/trending/movie/day';
+   const response = await axios.get(url,{
+      params:{
+         api_key:TMDB_API_KEY
+      },
+      timeout:5000
    })
+   const trendMovies =response.data.results;
 
-   const moviesArr = response.data.results;
-   await Promise.all(moviesArr.map(async (movie) =>{
-    return(
-     await MovieModel.create({
-        title:movie.title,
-        tmdb_id:movie.id,
-        type:movie.media_type,
+   await Promise.all(trendMovies.map(async (movie)=>{
+      const oldMovie= await MovieModel.findOne({tmdb_id:movie.id })
+      if(!oldMovie){
+       await MovieModel.create({
+        title :movie.title,
+        tmdb_id :movie.id,
+        type :"movie",
+        overview:movie.overview,
         release_date:movie.release_date,
         poster_path:movie.poster_path,
-        overview:movie.overview,
-        rating:movie.vote_average
-    }));
+        rating:movie.vote_average,
+       })
+      }
    }))
-   
-   res.json({
-    movies:moviesArr
-   })
-})
-
-movieRouter.get('/id',async (req,res)=>{
-     const movieId = req.query.movieId;
-     const movie = await MovieModel.findOne({
-       tmdb_id:movieId
-     });
-     res.json({
-        movieDetails:movie
-     })
-})
-
-movieRouter.get('/id/streaming',async (req,res)=>{
-     const movieTitle = req.query.movieTitile;
-     const country=req.query.country;
-  
-  const response= await axios.get('https://streaming-availability.p.rapidapi.com/shows/search/title',{
-   params: {
-        title: movieTitle,          
-        country: country,           
-        show_type: 'movie',        
-        output_language: 'en' 
-   },
-    headers: {
-    'x-rapidapi-key': RAPID_API_KEY,
-    'x-rapidapi-host': 'streaming-availability.p.rapidapi.com'
-    },
-   timeout:5000
+    res.json({
+      trendmovies:trendMovies
     })
-  console.log(response.data)
-
 })
 
 module.exports ={
