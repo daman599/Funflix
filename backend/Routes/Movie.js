1043160e79effed470acada8799dce298f43699b
daman599 <damanpreetk117@gmail.com>
2025-05-08 23:53:27 +0530

    search and movie details endpoint added

--- backend/Routes/Movie.js
+++ backend/Routes/Movie.js
@@ -7,6 +7,7 @@ const axios = require("axios");
 const { MovieModel } = require('../db');
 
 movieRouter.get('/trending',async (req,res)=>{
+
    const url='https://api.themoviedb.org/3/trending/movie/day';
    const response = await axios.get(url,{
       params:{
@@ -35,7 +36,57 @@ movieRouter.get('/trending',async (req,res)=>{
     })
 })
 
+movieRouter.get('/search',async(req,res)=>{
+   const title=req.query.title;
+   const found = await MovieModel.find({
+      title: { $regex: title, $options: 'i' }
+   })
+   if(found){
+      return res.json({
+         movies:found
+      })
+   }
+
+   const url="https://api.themoviedb.org/3/search/movie"
+   const response = await axios.get(url,{
+      params:{
+         api_key:TMDB_API_KEY,
+         query:title
+      }
+   })
+   const results=response.data.results;
+   await Promise.all(results.map(async (movie)=>{
+      const exist = await MovieModel.findOne({tmdb_id:movie.id});
+      if(!exist){
+       await MovieModel.create({
+        title :movie.title,
+        tmdb_id :movie.id,
+        type :"movie",
+        overview:movie.overview,
+        release_date:movie.release_date,
+        poster_path:movie.poster_path,
+        rating:movie.vote_average,
+         })
+      }
+   }))
+   return res.json({
+     searchedContent:results
+   })
+})
+
+movieRouter.get('/details/',async (req,res)=>{
+  const movieId = req.query.movieId;
+  const movieDetails = await MovieModel.findOne({tmdb_id:movieId});
+  res.json({
+   moviedetails:movieDetails
+  })
+})
+
+//movieRouter.get('/streaming/availability/',(req,res)=>{
+  // const movieId = req.query.movieId;
+   
+//})
+
 module.exports ={
     movieRouter : movieRouter
 }
-
