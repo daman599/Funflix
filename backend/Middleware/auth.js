06e6d9d155846e9445645d815001966bd2d871fe
x.daman.x <168351935+daman599@users.noreply.github.com>
2025-06-12 12:23:53 +0530

    Update auth.js

--- backend/Middleware/auth.js
+++ backend/Middleware/auth.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 const JWT_KEY = process.env.JWT_KEY;
 
 function Userauthentication(req,res,next){
-   const token = req.cookies.token;
+   const authHeader = req.headers['authorization'];
+   const token = authHeader && authHeader.split(' ')[1];
+    
    if(!token){
     res.json({
         error:"You are not signed in"
