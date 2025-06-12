a5de1c38c794f0d59aca7f51c8a0e96683ded693
x.daman.x <168351935+daman599@users.noreply.github.com>
2025-06-12 15:21:23 +0530

    Update OAuthCallback.jsx

--- frontend/React/src/Pages/OAuthCallback.jsx
+++ frontend/React/src/Pages/OAuthCallback.jsx
@@ -1,12 +1,15 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect }  from "react";
 
 export function OAuthCallback() {
   const navigate = useNavigate();
-
-    const params = new URLSearchParams(window.location.search);
-    const token = params.get("token");
-    if (token) {
+  
+  useEffect(()=>{
+     const params = new URLSearchParams(window.location.search);
+     const token = params.get("token");
+     if (token) {
       localStorage.setItem("token", token);
       navigate("/stream"); 
     }
+  },[navigate]) 
 }
