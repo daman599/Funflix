1b585606269dbc38e01405cc746fb4ec0bb41b4d
x.daman.x <168351935+daman599@users.noreply.github.com>
2025-06-12 12:21:54 +0530

    Update Google_auth.js

--- backend/Routes/Google_auth.js
+++ backend/Routes/Google_auth.js
@@ -56,14 +56,7 @@ googleAuthRouter.get('/auth/callback', async (req, res) => {
         "userId": user._id
     }, JWT_KEY);
 
-    res.cookie("token", token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-        domain: "funflix-frontend.onrender.com",
-        path:"/",
-        maxAge: 24 * 60 * 60 * 1000 * 7
-    })
+    res.json({token:token});
     res.redirect("https://funflix-frontend.onrender.com/stream");
 })
 
