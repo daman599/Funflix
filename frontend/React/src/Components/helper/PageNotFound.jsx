a2d4004f909d278db1c001e12b76ca265550bfbb
daman599 <damanpreetk117@gmail.com>
2025-12-01 19:10:45 +0530

    few fixes

--- frontend/React/src/Components/helper/PageNotFound.jsx
+++ frontend/React/src/Components/helper/PageNotFound.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const PageNotFound = () => {
     return (
-        <div class="min-h-screen flex flex-col items-center justify-center text-white p-2">
+        <div class="min-h-screen flex flex-col items-center justify-center p-2">
             <span class="text-4xl md:text-5xl font-bold text-[#181d49] mb-2 md:mb-4">404</span>
             <p class="text-base md:text-xl text-gray-400 text-center mb-3 md:mb-5">
                 Sorry, the page you're looking for doesn't exist.
