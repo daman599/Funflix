da749dae83cc311b73e2483ce2a46af0f2646efb
daman599 <damanpreetk117@gmail.com>
2025-12-04 00:27:02 +0530

    fixed text color

--- frontend/React/src/Components/helper/ErrorBoundary.jsx
+++ frontend/React/src/Components/helper/ErrorBoundary.jsx
@@ -17,7 +17,7 @@ export class ErrorBoundary extends React.Component {
     render() {
         if (this.state.hasError) {
             return <div className="min-h-screen flex items-center justify-center text-center">
-                <p className="text-gray-300 text-lg sm:text-xl md:text-2xl font-semibold">
+                <p className="text-gray-600 text-lg sm:text-xl font-semibold">
                     Something went wrong, please try again later.
                 </p>
             </div>
